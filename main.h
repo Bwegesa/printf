c469723302cd3e724056e2f0f372dda75188ff5b
Lorraine Ogolla <kisangelorraine@gmail.com>
2023-03-28 06:06:23 -0700

    Main.h update

--- main.h
+++ main.h
@@ -18,7 +18,27 @@ typedef struct type
 } type_t;
 
 int _putchar(char);
+int (*get_function(const char *specifier))(va_list);
 int _printf(const char *, ...);
-void _printstring(char *s);
+int _strlen(char *);
+int print_rev(va_list args);
+int rot13(va_list args);
+int print_number(unsigned int n);
+int countDigits(unsigned int num);
+int countOctal(unsigned int num);
+int countBinary(unsigned int num);
+int print_b(va_list args);
+int print_o(va_list args);
+int print_d(va_list args);
+int print_x(va_list args);
+void print_lowerHex(unsigned int num, int *count);
+int print_X(va_list args);
+void print_upperHex(unsigned int num, int *count);
+int print_p(va_list args);
+int print_s(va_list args);
+int print_c(va_list args);
+int print_u(va_list args);
+int print_S(va_list args);
+
 
 #endif
