9c72e176a73380ecbc006db6caf59de59cbdc588
Lorraine Ogolla <kisangelorraine@gmail.com>
2023-03-28 06:13:42 -0700

    print_s file

--- print_s.c
+++ print_s.c
@@ -1,47 +1,22 @@
 #include "main.h"
 
-
 /**
- * print_S - prints a string & allows to display values that aren't ASCII
- * @S: string to print
- * Description: if out ASCII range print as \\x
- * @args: name of the list to check from
- * Return: number of chars printed
+ * print_s - print a string or char argument
+ * @args: va_list containing the string or char to print as the next element
+ *
+ * Return: the number of bytes printed
  */
-int print_S(va_list args)
-{
-	unsigned int i;
-	int j = 0, k;
-	char c = 'A' - ':';
-	char d[2];
-	char *s = va_arg(args, char *);
 
-	if (s == NULL)
-		s = "(null)";
-	for (i = 0; s[i]; i++)
+int print_s(va_list args)
+{
+	int j;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (j = 0; str[j]; j++)
 	{
-		if (s[i] < 32 || s[i] > 126)
-		{
-			_putchar('\\');
-			_putchar('x');
-			j += 2;
-			d[0] = s[i] / 16;
-			d[1] = s[i] % 16;
-			for (k = 0; k < 2; k++)
-			{
-				if (d[k] >= 10)
-					_putchar('0' + c + d[k]);
-				else
-					_putchar('0' + d[k]);
-			}
-			j += k;
-		}
-		else
-		{
-			_putchar(s[i]);
-			j++;
-		}
+		_putchar(str[j]);
 	}
 	return (j);
-
 }
