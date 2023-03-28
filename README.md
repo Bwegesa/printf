49270a6c427b66adb40d2813ea5483a1e4e7924c
Bwegesa <wegesamaroa.7@gmail.com>
2023-03-28 05:56:12 -0700

    printf

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-printf project	
+printf project
+1.Main.h-contains function prototype
+2._putchar prints a string
+3.get_function determines which function to use
+4.print_s prints a string or character arguments
+5.printf a function that produces output according to format	
--- main.h
+++ main.h
@@ -4,6 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+/**
+ * struct type - connect conversion specifiers with the correct print function
+ * @print: a function pointer for the print functions
+ * @identifier: the conversion specifier
+ */
+typedef struct type
+{
+	char *identifier;
+	int (*print)(va_list);
+} type_t;
 
 int _putchar(char);
 int _printf(const char *, ...);
